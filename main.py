59eeb57f380d1d5764e9abee398e641c945e7405
spekkie300 <speklapje300@gmail.com>
2025-10-22 22:25:26 +0200

    Implemented basic window opening with Pygame

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import pygame
+
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+
 def main():
-    print("Hello from asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.quit:
+                return
+
+        screen.fill(0x0000)
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
